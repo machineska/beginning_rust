d33b6067386bc3639be1e9e1b3a0bbc98a2fbac9
machineska <alternative.xen@gmail.com>
2022-12-28 16:00:40 +0800

    in rust we trust part 2

---
+++ 10_number_separator.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let float_with_separator = 11_000.545_001;
+    println!("float value {}", float_with_separator);
+    let int_with_separator = 60_000;
+    println!("int value {}", int_with_separator);
+}
